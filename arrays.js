2351f69e3e9d0c50d9f4836d75380c6eebb0628f
hsnayrus <suryanshjain.1309@gmail.com>
2024-07-15 11:38:59 -0700

    Learned a bit about arrays from Mosh's video

---
+++ arrays.js
@@ -0,0 +1,10 @@
+// Array of colors selected by the user
+let selectedColors = ["red", "green"];
+// Arrays in JS are 0-indexed
+console.log(selectedColors[0]);
+// The contents and length of an array are dynamic in JS, meaning it can change at runtime
+// Adds a new element to the array
+selectedColors[2] = "blue";
+console.log(selectedColors);
+selectedColors[5] = 1;
+console.log(selectedColors);
