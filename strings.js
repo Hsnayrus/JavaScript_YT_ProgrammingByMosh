0daed5c35a259cbf22f83fe1a45c82e738303ff8
hsnayrus <suryanshjain.1309@gmail.com>
2024-07-18 16:25:07 -0700

    Learned and tried out some string methods

---
+++ strings.js
@@ -0,0 +1,9 @@
+let myStr = "  Hello  ";
+
+// A bunch of string methods
+console.log(myStr.trim());
+console.log(myStr.toLowerCase());
+console.log(myStr.toUpperCase());
+console.log(myStr.trim().concat(" World!"));
+
+// Can learn more at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
